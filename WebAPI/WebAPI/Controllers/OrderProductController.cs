7fafa3707e0a82fe52845ee5241f1db30c29f060
Simonesiss <simona.radulea@gmail.com>
2020-08-12 18:49:47 +0300

    Add Basic Controllers

---
+++ WebAPI/WebAPI/Controllers/OrderProductController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Entities;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderProductController : ControllerBase
+    {
+        private readonly ShopDBContext _context;
+
+        public OrderProductController(ShopDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/OrderProduct
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<OrderProduct>>> GetOrderProducts()
+        {
+            return await _context.OrderProducts.ToListAsync();
+        }
+
+        // GET: api/OrderProduct/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<OrderProduct>> GetOrderProduct(int id)
+        {
+            var orderProduct = await _context.OrderProducts.FindAsync(id);
+
+            if (orderProduct == null)
+            {
+                return NotFound();
+            }
+
+            return orderProduct;
+        }
+
+        // PUT: api/OrderProduct/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutOrderProduct(int id, OrderProduct orderProduct)
+        {
+            /*
+            if (id != orderProduct.OrderId)
+            {
+                return BadRequest();
+            }
+            */
+
+            _context.Entry(orderProduct).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrderProductExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/OrderProduct
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<OrderProduct>> PostOrderProduct(OrderProduct orderProduct)
+        {
+            _context.OrderProducts.Add(orderProduct);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (OrderProductExists(orderProduct.OrderId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetOrderProduct", new { id = orderProduct.OrderId }, orderProduct);
+        }
+
+        // DELETE: api/OrderProduct/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<OrderProduct>> DeleteOrderProduct(int id)
+        {
+            var orderProduct = await _context.OrderProducts.FindAsync(id);
+            if (orderProduct == null)
+            {
+                return NotFound();
+            }
+
+            _context.OrderProducts.Remove(orderProduct);
+            await _context.SaveChangesAsync();
+
+            return orderProduct;
+        }
+
+        private bool OrderProductExists(int id)
+        {
+            return _context.OrderProducts.Any(e => e.OrderId == id);
+        }
+    }
+}
