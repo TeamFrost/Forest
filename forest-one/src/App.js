d3ba521f96afe38379f38fd53f08d24fedae033a
Simonesiss <simona.radulea@gmail.com>
2020-08-14 13:15:11 +0300

    Add carousel component

--- forest-one/src/App.js
+++ forest-one/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 import LandingPage from './pages/LandingPage';
 import Header from './components/Header';
-import landing from './img/landing.png';
+import Slideshow from './components/Carousel';
 
 function App() {
   return (
-    <div className="App" styles={{ backgroundImage: `url(${landing})` }}>
-        <Header />
+    <div className="App">
+      <Header />
+      <Slideshow />
     </div>
   );
 }
