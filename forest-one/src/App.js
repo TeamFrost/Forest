00331cff02807d36f62684f5519060f6e5395744
EdiOnetiu <edyonetiu@gmail.com>
2020-08-14 14:57:33 +0300

    Add Footer & Sigla

--- forest-one/src/App.js
+++ forest-one/src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 import LandingPage from './pages/LandingPage';
 import Header from './components/Header';
 import landing from './img/landing.png';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App" styles={{ backgroundImage: `url(${landing})` }}>
         <Header />
+        {/* <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/> */}
+        <Footer />
     </div>
   );
 }
--- forest-one/src/components/Header.jsx
+++ forest-one/src/components/Header.jsx
@@ -3,7 +3,9 @@ import Button from '@material-ui/core/Button';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import { makeStyles } from '@material-ui/core/styles';
 import header from "../img/header.png";
+import siglamica from "../img/siglamica.png";
 import CallIcon from '@material-ui/icons/Call';
+import { Paper } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -12,6 +14,7 @@ const useStyles = makeStyles((theme) => ({
         backgroundImage: `url(${header})`
     },
     button: {
+        fontSize: '1rem',
         position: "absolute",
         left: 320,
         top: 450,
@@ -22,7 +25,16 @@ const useStyles = makeStyles((theme) => ({
         backgroundImage: 'linear-gradient(to right, #8f3232, #ac4234, #c85533, #e26a2f, #f98226);',
         '&:hover': {
             background: 'linear-gradient(45deg, #e65252 30%, #ffa35c 90%)',
-        }
+        },  
+    },
+    paper: {
+        width: 350,
+        height: 100,
+        position: "absolute",
+        left: 305,
+        top: 80,
+        borderRadius: 20,
+        paddingTop: 30,
     }
 }));
 
@@ -31,14 +43,19 @@ function Header() {
     const classes = useStyles();
 
     return (
-        <div className={classes.root}>
-            <Button
-                variant="contained"
-                className={classes.button}
-                endIcon={<CallIcon />}
-            >
-                Contacteaza-ne acum
-      </Button>
+        <div>
+            <div className={classes.root}>
+                <Paper className={classes.paper}>
+                    < img src={siglamica} alt="siglamica" />
+                </Paper>
+                <Button
+                    variant="contained"
+                    className={classes.button}
+                    endIcon={<CallIcon />}
+                >
+                    Contactează-ne acum!
+                 </Button>
+            </div>
         </div>
     );
 }
