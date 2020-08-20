539bb22d804e84d7d3a46fcaed691f89bff9f937
Simonesiss <simona.radulea@gmail.com>
2020-08-20 16:38:10 +0300

    Add List component for Front Page

--- forest-one/src/App.js
+++ forest-one/src/App.js
@@ -2,17 +2,26 @@ import React from 'react';
 import './App.css';
 import LandingPage from './pages/LandingPage';
 import Header from './components/Header';
-import landing from './img/landing.png';
 import Footer from './components/Footer';
-import Slideshow from './components/Carousel';
 import InfoCard from "./components/InfoCard";
+import FirstCarouselApp from "./components/FirstCarousel/FirstCarouselApp";
+import SecondCarouselApp from "./components/SecondCarousel/SecondCarouselApp";
+import Text from "./components/Text";
+import TableList from "./components/TableList";
+
 
 function App() {
     return (<div className="App" >
-        <Header />
-        <Slideshow />
-        <InfoCard />
-        </div>
+        {/* <Header /> */}
+        {/* <FirstCarouselApp /> */}
+        {/* <InfoCard /> */}
+        {/* <Text /> */}
+        {/* <SecondCarouselApp /> */}
+        {/* <Footer /> */}
+        <TableList />
+        
+
+    </div>
     );
 }
 
---
+++ forest-one/src/components/TableList.jsx
@@ -0,0 +1,195 @@
+import React from "react";
+import { Paper, Typography } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import bulletTree from "../img/bullet-tree.png";
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        margin: 50,
+        padding: 15,
+        width: 400,
+        height: 600,
+        borderColor: "#2A530B",
+        border: '3px solid',
+    },
+    text: {
+        color: '#259821',
+        marginBottom: 15,
+    },
+    large: {
+        width: theme.spacing(5),
+        height: theme.spacing(5),
+        marginRight: 15,
+    },
+    list: {
+        marginLeft: 50
+
+    },
+    listItem: {
+
+    }
+}));
+
+function TableList() {
+
+    const classes = useStyles();
+
+    return (
+        <div>
+            <Paper variant="outlined" className={classes.paper}>
+                <Typography variant="h4" className={classes.text}>
+                    <Box fontWeight="fontWeightBold" m={1}>
+                        LEMN IEFTIN!
+                    </Box>
+                </Typography>
+                <List className={classes.list}>
+                    <ListItem className={classes.listItem}>
+                        <ListItemAvatar>
+                            <Avatar
+                                variant="square"
+                                src={bulletTree}
+                                className={classes.large}
+                            />
+                        </ListItemAvatar>
+                        <ListItemText>
+                            <Typography variant="body1">
+                                <Box fontWeight="fontWeightBold" m={1}>
+                                    SCÂNDURĂ/DULAP VRAC
+                                </Box>
+                            </Typography>
+                        </ListItemText>
+                    </ListItem>
+
+                    <ListItem className={classes.listItem}>
+                        <ListItemAvatar>
+                            <Avatar
+                                variant="square"
+                                src={bulletTree}
+                                className={classes.large}
+                            />
+                        </ListItemAvatar>
+                        <ListItemText>
+                            <Typography variant="body1">
+                                <Box fontWeight="fontWeightBold" m={1}>
+                                    SCÂNDURĂ DIMENSIONATĂ
+                                </Box>
+
+                            </Typography>
+                        </ListItemText>
+                    </ListItem>
+                    <ListItem className={classes.listItem}>
+                        <ListItemAvatar>
+                            <Avatar
+                                variant="square"
+                                src={bulletTree}
+                                className={classes.large}
+                            />
+                        </ListItemAvatar>
+                        <ListItemText>
+                            <Typography variant="body1">
+                                <Box fontWeight="fontWeightBold" m={1}>
+                                    DULAPI DIMENSIONAȚI
+                                </Box>
+
+                            </Typography>
+                        </ListItemText>
+                    </ListItem>
+                    <ListItem className={classes.listItem}>
+                        <ListItemAvatar>
+                            <Avatar
+                                variant="square"
+                                src={bulletTree}
+                                className={classes.large}
+                            />
+                        </ListItemAvatar>
+                        <ListItemText>
+                            <Typography variant="body1">
+                                <Box fontWeight="fontWeightBold" m={1}>
+                                    LĂTEȚI, RIGLE
+                                </Box>
+
+                            </Typography>
+                        </ListItemText>
+                    </ListItem>
+                    <ListItem className={classes.listItem}>
+                        <ListItemAvatar>
+                            <Avatar
+                                variant="square"
+                                src={bulletTree}
+                                className={classes.large}
+                            />
+                        </ListItemAvatar>
+                        <ListItemText>
+                            <Typography variant="body1">
+                                <Box fontWeight="fontWeightBold" m={1}>
+                                    PAZIE, GRINZI
+                                </Box>
+
+                            </Typography>
+                        </ListItemText>
+                    </ListItem>
+                    <ListItem className={classes.listItem}>
+                        <ListItemAvatar>
+                            <Avatar
+                                variant="square"
+                                src={bulletTree}
+                                className={classes.large}
+                            />
+                        </ListItemAvatar>
+                        <ListItemText>
+                            <Typography variant="body1">
+                                <Box fontWeight="fontWeightBold" m={1}>
+                                    GARD, LAMBRIURI
+                                </Box>
+
+                            </Typography>
+                        </ListItemText>
+                    </ListItem>
+                    <ListItem className={classes.listItem}>
+                        <ListItemAvatar>
+                            <Avatar
+                                variant="square"
+                                src={bulletTree}
+                                className={classes.large}
+                            />
+                        </ListItemAvatar>
+                        <ListItemText>
+                            <Typography variant="body1">
+                                <Box fontWeight="fontWeightBold" m={1}>
+                                    PLĂCI OSB 3
+                                </Box>
+
+                            </Typography>
+                        </ListItemText>
+                    </ListItem>
+                    <ListItem className={classes.listItem}>
+                        <ListItemAvatar>
+                            <Avatar
+                                variant="square"
+                                src={bulletTree}
+                                className={classes.large}
+                            />
+                        </ListItemAvatar>
+                        <ListItemText>
+                            <Typography variant="body1">
+                                <Box fontWeight="fontWeightBold" m={1}>
+                                    PRODUSE FINISATE
+                                </Box>
+                               
+                            </Typography>
+                        </ListItemText>
+                    </ListItem>
+
+                </List>
+            </Paper>
+        </div>
+    );
+}
+
+export default TableList;
