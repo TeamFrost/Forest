aaabb30b32738533536243b73a6af0f9a95c18ca
EdiOnetiu <edyonetiu@gmail.com>
2020-08-31 14:09:21 +0300

    Finish Advantages

--- forest-one/src/components/Advantages.jsx
+++ forest-one/src/components/Advantages.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 import AdvantagesPhoto from "../img/AdvantagesPhoto.png";
 import { Paper, Typography } from '@material-ui/core';
 import Box from '@material-ui/core/Box';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -15,7 +17,7 @@ const useStyles = makeStyles((theme) => ({
         top: 75,
         width: '100%',
         height: 650,
-        backgroundColor: "#FFFFFFE6"
+        backgroundColor: "#FFFFFFDE"
     },
     text: {
         paddingTop: 80,
@@ -24,8 +26,34 @@ const useStyles = makeStyles((theme) => ({
     },
     bodyText: {
         paddingTop: 30,
+    },
+    grid: {
+        marginTop: 30,
+    },
+    grid2:{
+        marginTop: 45,
     }
 }));
+
+const BorderLinearProgress = withStyles((theme) => ({
+    root: {
+        position: 'relative',
+        left: 50,
+        height: 50,
+        borderRadius: 5,
+        width: 780,
+        marginLeft: 50,
+        marginTop: 15
+    },
+    colorPrimary: {
+        backgroundColor: '#ebcea4',
+    },
+    bar: {
+        borderRadius: 5,
+        backgroundColor: '#D69942',
+        borderColor: '#D69942'
+    },
+}))(LinearProgress);
 
 function Advantages() {
 
@@ -46,6 +74,61 @@ function Advantages() {
                         făcând să fie folosit nu doar ca material structural, dar și ca material de finisaj.
                     </Box>
                 </Typography>
+                <Grid container spacing={6} className={classes.grid}>
+                    <Grid container xs={12} sm={6} className={classes.grid2}>
+                        <Grid item xs={12} sm={9}>
+                            <Box fontWeight={"fontWeightLight"} fontSize={23}>
+                                Cel mai bun material izolant natural termic și fonic &nbsp;&nbsp;&nbsp;&nbsp;
+                            </Box>
+                        </Grid>
+                        <Grid item xs={12} sm={3}>
+                            <Box fontWeight={"Semi-Bold"} fontSize={23}>
+                                100%
+                            </Box>
+                        </Grid>
+                        <BorderLinearProgress variant="determinate" value={100} />
+                    </Grid>
+                    <Grid container xs={12} sm={6} className={classes.grid2}>
+                        <Grid item xs={12} sm={9}>
+                            <Box fontWeight={"fontWeightLight"} fontSize={23}>
+                            Lemnul se poate prelucra și fasona relativ ușor&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            </Box>
+                        </Grid>
+                        <Grid item xs={12} sm={3}>
+                            <Box fontWeight={"Semi-Bold"} fontSize={23}>
+                                75%
+                            </Box>
+                        </Grid>
+                        <BorderLinearProgress variant="determinate" value={75} />
+                    </Grid>
+                    <Grid container xs={12} sm={6} className={classes.grid2}>
+                        <Grid item xs={12} sm={9}>
+                            <Box fontWeight={"fontWeightLight"} fontSize={23}>
+                            Element valoros în designul de interior și exterior &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+                            </Box>
+                        </Grid>
+                        <Grid item xs={12} sm={3}>
+                            <Box fontWeight={"Semi-Bold"} fontSize={23}>
+                                90%
+                            </Box>
+                        </Grid>
+                        <BorderLinearProgress variant="determinate" value={90} />
+                    </Grid>
+                    <Grid container xs={12} sm={6} className={classes.grid2}>
+                        <Grid item xs={12} sm={9}>
+                            <Box fontWeight={"fontWeightLight"} fontSize={23}>
+                            Durabilitatea mare a construcțiilor din lemn&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  
+                            </Box>
+                        </Grid>
+                        <Grid item xs={12} sm={3}>
+                            <Box fontWeight={"Semi-Bold"} fontSize={23}>
+                                85%
+                            </Box>
+                        </Grid>
+                        <BorderLinearProgress variant="determinate" value={85} />
+                    </Grid>
+                </Grid>
             </Paper>
         </div>
     );
