4ab6b7693bdd85da21721e314474b8af977a06f5
EdiOnetiu <edyonetiu@gmail.com>
2020-09-01 19:34:21 +0300

    Finish Footer & Social Icons

--- forest-one/src/components/Footer.jsx
+++ forest-one/src/components/Footer.jsx
@@ -10,6 +10,12 @@ import AccessTimeIcon from '@material-ui/icons/AccessTime';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
+import Facebook from "../img/facebook.svg";
+import Youtube from "../img/youtube.svg";
+import Blogger from "../img/blogger.svg";
+import Location from "../img/social-media.svg";
+import Whatsapp from "../img/whatsapp.svg";
+import Houzz from "../img/houzz.svg";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -24,12 +30,19 @@ const useStyles = makeStyles((theme) => ({
         backgroundColor: 'rgb(49 126 46)',
         color: 'white'
     },
-    paper2:{
+    paper2: {
         position: 'relative',
-        top:103,
+        top: 103,
         width: '60%',
         backgroundColor: 'rgb(49 126 46)',
         color: 'white'
+    },
+    paper3: {
+        position: 'relative',
+        top: 50,
+        left: 1450,
+        backgroundColor: 'rgb(49 126 46)',
+        width: '25%',
     },
     icon: {
         fontSize: 90,
@@ -50,17 +63,23 @@ const useStyles = makeStyles((theme) => ({
         fontSize: 20,
         fontWeight: "semi-bold",
     },
-    info3:{
+    info3: {
         textAlign: 'center',
         color: '#E59E39',
         fontWeight: "semi-bold",
         fontSize: 17,
-        paddingTop:5
+        paddingTop: 5
     },
     specialText: {
-        color:'white',
+        color: 'white',
+    },
+    icons: {
+        width: 40,
+        marginLeft: 15,
+        cursor: 'pointer'
     }
 }));
+
 function Footer() {
 
     const classes = useStyles();
@@ -90,7 +109,7 @@ function Footer() {
                 </Grid>
             </Paper>
             <Paper elevation={0} className={classes.paper2}>
-            <Grid container spacing={3}>
+                <Grid container spacing={3}>
                     <Grid item xs={12} sm={2}></Grid>
                     <Grid item xs={12} sm={1}><AccessTimeIcon className={classes.icon} /></Grid>
                     <Grid item xs={12} sm={3}>
@@ -112,6 +131,32 @@ function Footer() {
                         </List>
                     </Grid>
                 </Grid>
+            </Paper>
+            <Paper elevation={0} className={classes.paper3}>
+                <img src={Facebook} className={classes.icons} onClick={(e) => {
+                    e.preventDefault();
+                    window.open("https://www.facebook.com/vreaulemn/");
+                }} />
+                 <img src={Youtube} className={classes.icons} onClick={(e) => {
+                    e.preventDefault();
+                    window.open("https://www.youtube.com/channel/UCy3UnLiHkerLlaVsm_pwwrg");
+                }} />
+                <img src={Location} className={classes.icons} onClick={(e) => {
+                    e.preventDefault();
+                    window.open("https://www.google.com/maps/@45.7234705,21.2963809,3a,75y,144.71h,89.09t/data=!3m6!1e1!3m4!1sW_1J7EpjBjc1fIDaB1hz7Q!2e0!7i13312!8i6656?hl=en-US");
+                }} />
+                <img src={Whatsapp} className={classes.icons} onClick={(e) => {
+                    e.preventDefault();
+                    window.open("https://www.google.com");
+                }} />
+                 <img src={Blogger} className={classes.icons} onClick={(e) => {
+                    e.preventDefault();
+                    window.open("http://vreaulemn.blogspot.com/p/lemnul-de-constructii-1.html");
+                }} />
+                <img src={Houzz} className={classes.icons} onClick={(e) => {
+                    e.preventDefault();
+                    window.open("https://www.houzz.com/professionals/home-builders/forest-one-pfvwus-pf~1511217594?");
+                }} />
             </Paper>
         </div>
     );
